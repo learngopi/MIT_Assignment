a1e9c34f2957c19ff0cef5f80631d87f4f1247b1
gramalin <ramalingam@un.org>
2022-05-29 19:30:38 +0700

    Update Navbar.jsx

--- todos/src/Navbar.jsx
+++ todos/src/Navbar.jsx
@@ -1,9 +1,13 @@
 import { Button } from 'react-bootstrap';
 
+  function handleClick(e) {
+    alert(`hey! you clicked: ${e.target.innerHTML}`);
+  }
+
 const Navbar = () => {
 const menuitems = ['File','Tools','Help'];
 const updatedlist = menuitems.map((menuitem, index ) => {
-    return <Button variant="light" size="lg" key={index}>{menuitem}</Button>
+    return <Button variant="light" size="lg" key={index} onClick={(e) => handleClick(e)} >{menuitem}</Button>
 });
     return <ul>{updatedlist}</ul>
 }
