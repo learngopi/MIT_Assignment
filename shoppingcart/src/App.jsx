f1b34810fd17e84611a24474d46c6c0c50669efd
gramalin <ramalingam@un.org>
2022-05-30 09:55:54 +0700

    cart component

---
+++ shoppingcart/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import './cart';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Shopping cart application
+      </header>
+      <div className='App-body'>Application in progress</div>
+    </div>
+  );
+}
+export default App;
---
+++ shoppingcart/src/cart.jsx
@@ -0,0 +1,8 @@
+
+import { Button } from 'react-bootstrap';
+
+const Cart = () => {
+
+}
+
+export default Cart;
